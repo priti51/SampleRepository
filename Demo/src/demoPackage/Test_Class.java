4763ac666b0fb2675a57e984633bb33aa4ffc29a
Priti Mohite <mohitepriti2409@gmail.com>
2023-01-30 12:24:06 +0530

    Demo Project

---
+++ Demo/src/demoPackage/Test_Class.java
@@ -0,0 +1,18 @@
+package demoPackage;
+
+public class Test_Class {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Github.com");
+		System.out.println("Create sample repository");
+		System.out.println("publish repository");
+		System.out.println("Work completed");
+		
+		
+		
+	}
+	
+	
+	
+}
